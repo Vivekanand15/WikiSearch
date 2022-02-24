512a0de061dd7bc731a393f6a51bc9a1bdf2b297
Vivekanand <vivekanand.v@surya-soft.com>
2022-02-25 01:34:27 +0530

    we just made the axios request

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,13 +1,24 @@
 import React,{ useState , useEffect} from 'react';
+import axios from 'axios';
 
 const Search =()=>{
     const [term,setTerm]=useState("");
-    console.log('i will run every time');
-    useEffect(()=>{
-        console.log('i will run only once');
-    });
+    useEffect( ()=>{
+        const search=async ()=>{
+            await axios.get('https://en.wikipedia.org/w/api.php',{
+                params:{
+                    action:'query',
+                    list:'search',
+                    origin:'*',
+                    format:'json',
+                    srsearch:term,
+                }
+            });
+        };
+        search();
+    },[term]);
     
-    return (
+    return(
         <div>
             <div className="ui form ">
                 <div className='field'>
